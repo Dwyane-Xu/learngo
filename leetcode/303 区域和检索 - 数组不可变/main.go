2abe1de89719eaa1a5b3e5c900176ff3a47ae794
xujinzhao <502794297@qq.com>
2020-05-07 21:55:28 +0800

    add leetcode

---
+++ leetcode/303 区域和检索 - 数组不可变/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type NumArray struct {
+	value []int
+}
+
+func Constructor(nums []int) NumArray {
+	// numa := NumArray{[]int{0}}
+	// for
+	return NumArray{}
+}
+
+func (this *NumArray) SumRange(i int, j int) int {
+	return 1
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(i,j);
+ */
+
+func main() {
+	r := [...]int{15: 10}
+	fmt.Println(r)
+}
